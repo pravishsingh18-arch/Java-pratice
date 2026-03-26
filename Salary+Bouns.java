db042d3625cbbd739475dbd5f20607de31e368d1
pravish kumar singh <pravishsingh18@gmail.com>
2026-03-26 14:20:05 +0530

    Add SalaryBonus class to calculate total salary
    this program calculates total salary by adding basic  salary and bouns
    salary = 20000
    bouns = 5000
    total salary is printed as output

---
+++ Salary+Bouns.java
@@ -0,0 +1,12 @@
+public class SalaryBonus {
+
+    public static void main(String[] args) {
+
+        int salary = 20000;
+        int bonus = 5000;
+
+        int total = salary + bonus;
+
+        System.out.println("Total Salary = " + total);
+    }
+}
