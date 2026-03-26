ec5d67759ea8904d4672d4535c03d8a5fb9d4d92
pravish kumar singh <pravishsingh18@gmail.com>
2026-03-26 14:29:49 +0530

    Correct syntax issues in multiplynumbers.java
    Fix syntax errors in multiplynumbers.java.

---
+++ multiplynumbers.java
@@ -0,0 +1,10 @@
+public class multiplynumbers {
+    public ststic void main(string[]args)
+{
+        int a = 5;
+        int b = 4;
+        int result = a * b;
+        system.out.println("multiplication="+result);
+   }
+       
+{
