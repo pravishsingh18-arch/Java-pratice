cb5e9c4cc76db3ea47fd48219f06e4e0daf5add6
pravish kumar singh <pravishsingh18@gmail.com>
2026-03-26 14:26:11 +0530

    Add SquareCube class to calculate square and cube

---
+++ square cube.java
@@ -0,0 +1,14 @@
+public class SquareCube {
+
+    public static void main(String[] args) {
+
+        int num = 5;
+
+        int square = num * num;
+        int cube = num * num * num;
+
+        System.out.println("Number = " + num);
+        System.out.println("Square = " + square);
+        System.out.println("Cube = " + cube);
+    }
+}
