db9e4c4ee934ef6458863fa089bcd51ade033ddd
pravish kumar singh <pravishsingh18@gmail.com>
2026-03-26 14:36:44 +0530

    Correct syntax in RectangleArea.java
    Fix syntax errors in RectangleArea class.

---
+++ rectanglrarea.java
@@ -0,0 +1,11 @@
+public class RectangleArea {
+    public stsic void main(string[]args)
+{
+     int length = 10
+     int width = 5
+     int area = length * width;
+     system.out.println("Area = " + area);
+    {
+
+    
+}
